2754cd4a1ecc67890c87bb132e34d55d17abac49
eroliver <eroliver27@hotmail.com>
2022-07-05 03:03:13 -0400

    Switched completely to pyautogui instead of using selenium. Grabbed a screen shot of the empty box that letters go into for the game. After navigating to the web page, this image is used to find the coordinates for all the letter locations. Once we have these, a guess at the word can be made. Wordle will mark whether the letters are correct or not for that word. Using the locations grabbed previously, I check the colors of the boxes to see whether the letters are in the word or not. Each letter can be added to the set of letters that are known not to be in the word, or to the list of sets to track letters that we have found incorrect locations for. I just need to set up the algorithm to consistently get the right word. I'm thinking the first two words can be static guesses that elimate the most common words and letters quickly to narrow the dictionary the most.

---
+++ automaticWordle/get_color.py
@@ -0,0 +1,15 @@
+from pyautogui import pixel, screenshot
+from time import sleep 
+
+left = 1004
+top = 290
+pixel = pixel(left, top)
+
+print(pixel)
+
+left = 20
+top = 20
+pixel = pixel(left, top)
+
+print(pixel)
+
--- automaticWordle/wordle_bot.py
+++ automaticWordle/wordle_bot.py
@@ -1,11 +1,33 @@
-from pyautogui import write, press, leftClick, screenshot
+from pyautogui import write, press, leftClick, screenshot, locateAllOnScreen, FAILSAFE, PAUSE, pixel, pixelMatchesColor, typewrite
 from time import sleep
+
+# Colors for checking if letters are correct
+# not in word = 120, 124, 126
+# in wrong location = 201, 180, 88
+# correct letter, correct spot = 106, 170, 100
+
+PAUSE = 3
+FAILSAFE = True
+
+wrong_letter = tuple((120, 124, 126))
+correct_letter = tuple((106, 170, 100))
+misplaced_letter = tuple((201, 180, 88))
+
+#guesses = 0
+
+first_guess = 'years'
+second_guess = 'timed'
+third_guess = 'group'
+fourth_guess = 'quest'
+
+not_in_word = set()
+not_in_position = [set(), set(), set(), set(), set()]
 
 press('win')
 
 sleep(2)
 
-write('chrome')
+write('"C:\Program Files\Google\Chrome\Application\chrome.exe" -incognito')
 
 sleep(2)
 
@@ -21,13 +43,53 @@ sleep(3)
 
 leftClick(500, 500)
 
-write('years')
+sleep(5)
+
+letter_boxes = []
+
+letter_boxes = [((location.left + 7 ), (location.top + 7)) for location in locateAllOnScreen('letter_box.png')]
+
+typewrite(first_guess, 0.1)
+
+sleep(1)
 
 press('enter')
 
+sleep(3)
+
+def Check_Correct_Letters(guesses, guess):
+    for position in range(0, 5):
+        left = letter_boxes[position][0]
+        top = letter_boxes[position][1]
+        print(left, top)
+        if (pixel(int(left), int(top)) == wrong_letter):
+            not_in_word.add(guess[position])
+            print(f'{guess[position]} not in word')
+        elif (pixel(int(left), int(top)) == misplaced_letter):
+            not_in_position[position].add(guess[position])
+            print(f'{guess[position]} not in {position}')
+    del letter_boxes[0:5]
+
+# for coordinate in letter_boxes:
+#     left = coordinate[0]
+#     top = coordinate[1]
+#     print((pixel(int(left), int(top))))
+
+#press('enter')
+
+Check_Correct_Letters(0, first_guess)
 
 sleep(3)
 
-screen = screenshot()
-screen.save('first_guess.png')
+typewrite(second_guess, 0.1)
 
+sleep(1)
+
+press('enter')
+
+sleep(3)
+
+Check_Correct_Letters(1, second_guess)
+
+print(not_in_word)
+print(not_in_position)
