8f6cffdb9f8b9f8374434d7005175063e22f8325
eroliver <eroliver27@hotmail.com>
2022-07-08 03:14:09 -0400

    Finished implementing the methods to reduce the list after each guess, so now the word list is reduced by removing 1. all words that don't have a correct letter in the position it was marked correct in 2. all words that have a letter marked as misplaced in the position it was marked misplaced in and 3. all words that contain a letter marked grey, unless it is in one of the other lists, which could be improved by removing words, which have a duplicate in them, but who can be bothered to do that? It still needs cleaned up quite a bit, but works farely well. Oh, since the wordlist includes words that wordle does not accept, I added a method to clear out the guess and guess again if a word is guessed that isn't accepted.

--- automaticWordle/wordle_bot.py
+++ automaticWordle/wordle_bot.py
@@ -1,56 +1,49 @@
+from enum import Flag
 from pip import main
 from pyautogui import write, press, leftClick, screenshot, locateAllOnScreen, FAILSAFE, PAUSE, pixel, pixelMatchesColor, typewrite
 from time import sleep
 from random import randint
 
 def main():
-    # https://github.com/madalynrose/Words/blob/master/assets/5-letter.txt is where the word list came from.
-    # Colors for checking if letters are correct
-    # not in word = 120, 124, 126
-    # in wrong location = 201, 180, 88
-    # correct letter, correct spot = 106, 170, 100
-
-    PAUSE = 3
+    # When fail-safe mode is True, moving the mouse to the upper-left will raise a pyautogui.FailSafeException that can abort your program:
     FAILSAFE = True
 
     wrong_letter = tuple((120, 124, 126))
     correct_letter = tuple((106, 170, 100))
     misplaced_letter = tuple((201, 180, 88))
 
-    #guesses = 0
-
-    first_guess = 'trace'
-    second_guess = 'timed'
-    third_guess = 'group'
+    # guesses = 0
+    # these two guesses should reduce the word list drastically 
+    first_guess = 'scale'
+    second_guess = 'group'
+    # third-sixth will be replaced 
+    third_guess = 'loser'
     fourth_guess = 'quest'
     fifth_guess = 'sorry'
-
-    def import_word_list():
-        with open('all_words.txt') as text:
-            words = text.read()
-            stripped = words.strip('[]')
-            replaced = stripped.replace("'","")
-            word_list = replaced.split(', ')
-            return(word_list)
-
-    word_list = import_word_list()
-
+    # import a list of 5 letter words, there's no check on the words, so they need to be 5 letters long
+    word_list = import_word_list('all_words.txt')
+    # letters that wordle marks grey will be added here
     not_in_word = set()
-    removed_from_list = set()
+    # this list contains the letters that get marked yellow for a given position
     not_in_position = [set(), set(), set(), set(), set()]
-
+    # These are the green marked letters, though idk why I didn't just make this a str...
+    correct_letters = ['0', '0', '0', '0', '0']
+    # lazily corrected method for removing letters if they are marked grey
     def check_words(words: list) -> list:
         better_words = []
+        somewhere = ''
+        for positions in not_in_position:
+            somewhere += str(positions)
         for word in words:
             bad = False
             for letter in not_in_word:
-                if letter in word:
+                if letter in word and letter not in correct_letters and letter not in somewhere:
                     bad = True
             if not bad:
                 better_words.append(word)
         not_in_word.clear()
         return better_words
-
+    # removes words that have a yellow letter in the position they were marked yellow in
     def check_letter_positions(words: list, not_in_position: set) -> list:
         better_words = []
         for word in words:
@@ -62,8 +55,135 @@ def main():
                 better_words.append(word)
         not_in_position = [set(), set(), set(), set(), set()]
         return better_words
-                
+    # removes words from the list that do not have a letter marked green in the marked position
+    def check_correct_positions(words: list, correct_letters: list) -> list:
+        better_words = []
+        for word in words:
+            bad = False
+            for idx, letter in enumerate(correct_letters):
+                if letter != '0':
+                    if word[idx] != letter:
+                        bad = True
+            if not bad:
+                better_words.append(word)
+        return better_words
 
+    open_wordle_in_incognito()
+
+    sleep(2)
+
+    letter_boxes = get_letter_locations()
+
+    make_guess(first_guess)
+
+    def Check_Correct_Letters(guess):
+        is_valid = True
+        for position in range(0, 5):
+            left = letter_boxes[position][0]
+            top = letter_boxes[position][1]
+            if (pixel(int(left), int(top)) == wrong_letter):
+                not_in_word.add(guess[position])
+            elif (pixel(int(left), int(top)) == misplaced_letter):
+                not_in_position[position].add(guess[position])
+            elif (pixel(int(left), int(top)) == correct_letter):
+                correct_letters[position] = guess[position]
+            else:
+                press('backspace', presses=5)
+                is_valid = False
+        if is_valid:
+            del letter_boxes[0:5]
+
+    Check_Correct_Letters(first_guess)
+
+    # update wordlist based on letters not in the word.
+    # not sure the best way to do this quickly. checking every word with every letter will be pretty slow.
+    word_list = check_words(word_list)
+    print(len(word_list))
+
+    word_list = check_letter_positions(word_list, not_in_position)
+    print(len(word_list))
+
+    word_list = check_correct_positions(word_list, correct_letters)
+    print(len(word_list))
+
+    #if len(word_list) > 0:
+    #    second_guess = word_list[randint(0,(len(word_list) - 1))]
+    
+    make_guess(second_guess)
+
+    Check_Correct_Letters(second_guess)
+
+    word_list = check_words(word_list)
+    word_list = check_letter_positions(word_list, not_in_position)
+    word_list = check_correct_positions(word_list, correct_letters)
+
+    print(len(word_list))
+
+    if len(word_list) > 0:
+        third_guess = word_list[randint(0,(len(word_list) - 1))]
+
+    make_guess(third_guess)
+
+    Check_Correct_Letters(third_guess)
+
+    word_list = check_words(word_list)
+    word_list = check_letter_positions(word_list, not_in_position)
+    word_list = check_correct_positions(word_list, correct_letters)
+
+    print(len(word_list))
+
+    if len(word_list) > 0:
+        forth_guess = word_list[randint(0,(len(word_list) - 1))]
+    
+    make_guess(forth_guess)
+
+    Check_Correct_Letters(forth_guess)
+
+    word_list = check_words(word_list)
+    word_list = check_letter_positions(word_list, not_in_position)
+    word_list = check_correct_positions(word_list, correct_letters)
+
+    print(len(word_list))
+
+    if len(word_list) > 0:
+        fifth_guess = word_list[randint(0,(len(word_list) - 1))]
+    
+    make_guess(fifth_guess)
+
+    Check_Correct_Letters(fifth_guess)
+
+    word_list = check_words(word_list)
+    word_list = check_letter_positions(word_list, not_in_position)
+    word_list = check_correct_positions(word_list, correct_letters)
+
+    if len(word_list) > 0:
+        sixth_guess = word_list[randint(0,(len(word_list) - 1))]
+
+    make_guess(sixth_guess)
+    print(word_list)
+
+def make_guess(word_guess):
+    typewrite(word_guess, 0.1)
+
+    sleep(1)
+
+    press('enter')
+
+    sleep(3)
+
+def import_word_list(words_file: str) -> list:
+    with open(words_file) as text:
+        words = text.read()
+        stripped = words.strip('[]')
+        replaced = stripped.replace("'","")
+        word_list = replaced.split(', ')
+        return(word_list)
+
+def get_letter_locations() -> list:
+    letter_boxes = [((location.left + 7 ), (location.top + 7)) for location in locateAllOnScreen('letter_box.png')]
+    return letter_boxes
+# pyautogui commands to open chrome in incognito and navigate to wordle as well as click on the screen to close the tutorial popup
+def open_wordle_in_incognito():
     press('win')
 
     sleep(2)
@@ -84,112 +204,5 @@ def main():
 
     leftClick(500, 500)
 
-    sleep(5)
-
-    letter_boxes = []
-
-    letter_boxes = [((location.left + 7 ), (location.top + 7)) for location in locateAllOnScreen('letter_box.png')]
-
-    typewrite(first_guess, 0.1)
-
-    sleep(1)
-
-    press('enter')
-
-    sleep(3)
-
-    def Check_Correct_Letters(guesses, guess):
-        for position in range(0, 5):
-            left = letter_boxes[position][0]
-            top = letter_boxes[position][1]
-            if (pixel(int(left), int(top)) == wrong_letter):
-                not_in_word.add(guess[position])
-            elif (pixel(int(left), int(top)) == misplaced_letter):
-                not_in_position[position].add(guess[position])
-        del letter_boxes[0:5]
-
-
-    # for coordinate in letter_boxes:
-    #     left = coordinate[0]
-    #     top = coordinate[1]
-    #     print((pixel(int(left), int(top))))
-
-    #press('enter')
-
-    Check_Correct_Letters(0, first_guess)
-
-    # update wordlist based on letters not in the word.
-    # not sure the best way to do this quickly. checking every word with every letter will be pretty slow.
-    word_list = check_words(word_list)
-    word_list = check_letter_positions(word_list, not_in_position)
-    print(len(word_list))
-    if len(word_list) > 0:
-        second_guess = word_list[randint(0,(len(word_list) - 1))]
-    typewrite(second_guess, 0.1)
-
-    sleep(1)
-
-    press('enter')
-
-    sleep(3)
-
-    Check_Correct_Letters(1, second_guess)
-
-    word_list = check_words(word_list)
-    word_list = check_letter_positions(word_list, not_in_position)
-
-    print(len(word_list))
-
-    if len(word_list) > 0:
-        third_guess = word_list[randint(0,(len(word_list) - 1))]
-    typewrite(third_guess, 0.1)
-
-    sleep(1)
-
-    press('enter')
-
-    sleep(3)
-
-    Check_Correct_Letters(1, third_guess)
-
-    word_list = check_words(word_list)
-    word_list = check_letter_positions(word_list, not_in_position)
-
-    print(len(word_list))
-
-    if len(word_list) > 0:
-        forth_guess = word_list[randint(0,(len(word_list) - 1))]
-    typewrite(forth_guess, 0.1)
-
-    sleep(1)
-
-    press('enter')
-
-    sleep(3)
-
-    Check_Correct_Letters(1, forth_guess)
-
-    word_list = check_words(word_list)
-    word_list = check_letter_positions(word_list, not_in_position)
-
-    print(len(word_list))
-
-    if len(word_list) > 0:
-        fifth_guess = word_list[randint(0,(len(word_list) - 1))]
-    typewrite(fifth_guess, 0.1)
-
-    sleep(1)
-
-    press('enter')
-
-    sleep(3)
-
-    Check_Correct_Letters(1, fifth_guess)
-
-    word_list = check_words(word_list)
-    word_list = check_letter_positions(word_list, not_in_position)
-
-    print(word_list)
-    
 if __name__ == '__main__':
     main()
